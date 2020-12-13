acd7e7ae4fd85202cd0512632f04ab6e4059471f
Oliver Steele <69857520+ogsteele@users.noreply.github.com>
2020-12-13 22:24:07 +0000

    namespace and documentation

--- R/theme_test_OGS.R
+++ R/theme_test_OGS.R
@@ -1,3 +1,25 @@
+#' Prism-esque Plots
+#'
+#' This is just a test version of a theme, looks like Prism plots.
+#'
+#' Properties as follows:
+#'
+#'       legend.position = "none"
+#'
+#'       plot.title = element_text(hjust = 0.5, vjust = 4, face = "bold", size = 16)
+#'
+#'       axis.line = element_line(size = 1.4)
+#'
+#'       axis.text.x = element_text(face = "italic", size = 14)
+#'
+#'       axis.text.y = element_text(size = 12)
+#'
+#'       plot.margin = unit(c(1,2,1,2),"cm")
+#' @export
+#' @examples
+#' "plot <- ggplot(x,y,data) + theme_test_OGS()"
+#'
+
 theme_test_OGS <- function () {
   ggplot2::theme_classic(base_size=14, base_family="Arial") %+replace%
     theme( # change stuff here
